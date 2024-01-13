9453ffdff2061c198d60973394737a6e16080f7c
martinho <martinho.n03@gmail.com>
2024-01-13 20:44:39 +0200

    add some comments

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// find the paragraph element with the class date
 const date = document.querySelector('.date');
 
+// Gets the year, using local time, and sets it to the text content of the date element
 date.textContent = new Date().getFullYear();
 
+// find the button element with the id actionButton and add an event listener
 document.getElementById("actionButton").addEventListener("click", function() {
     const p = document.querySelector('#description');
+    // check if the paragraph is hidden
     if (p.style.visibility === 'hidden') {
         p.style.visibility = 'visible';
     } else {
